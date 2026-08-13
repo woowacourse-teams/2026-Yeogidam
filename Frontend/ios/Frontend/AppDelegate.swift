962693be5dcf69986e283dba4c53c10022daba7c
nadajinny <nadajinny@gmail.com>
2026-08-13 20:58:40 +0900

    feat: iOS Share Extension 구현

--- Frontend/ios/Frontend/AppDelegate.swift
+++ Frontend/ios/Frontend/AppDelegate.swift
@@ -43,6 +43,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   return true
 }
+
+  func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    let isShareExtensionURL = handleShareExtensionURL(url)
+    let isLinkingURL = RCTLinkingManager.application(app, open: url, options: options)
+
+    return isShareExtensionURL || isLinkingURL
+  }
+
+  func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    RCTLinkingManager.application(
+      application,
+      continue: userActivity,
+      restorationHandler: restorationHandler
+    )
+  }
+
+  private func handleShareExtensionURL(_ url: URL) -> Bool {
+    guard
+      url.scheme == "com.yeogidamm.app",
+      url.host == ShareIntentConstants.shareExtensionHost
+    else {
+      return false
+    }
+
+    ShareIntentModule.notifyPendingShareAvailable()
+    return true
+  }
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
